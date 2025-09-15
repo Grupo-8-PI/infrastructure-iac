a9d79ff52c1ffd6132083bf62099e3ccf55b2947
FelipeGasp <felipe.gasparotto@sptech.school>
2025-09-15 18:50:29 -0300

    feat: added ssh connection to each instance

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,3 +1,21 @@
+resource "tls_private_key" "ec2_ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "ec2_key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.ec2_ssh_key.public_key_openssh
+}
+
+output "private_key_pem" {
+  value     = tls_private_key.ec2_ssh_key.private_key_pem
+  sensitive = true
+}
+
+output "key_name" {
+  value = aws_key_pair.ec2_key_pair.key_name
+}
 # Variables
 variable "ami_id" {
   description = "ID da AMI para as instâncias EC2"
